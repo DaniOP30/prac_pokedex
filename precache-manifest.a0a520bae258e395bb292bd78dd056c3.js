1f92fc4141e015332db87bd38711e344d0235dcd
Daniel Olivo <68250690+DaniOP30@users.noreply.github.com>
2021-10-11 02:27:18 -0500

    deploy

---
+++ precache-manifest.a0a520bae258e395bb292bd78dd056c3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f2194ef86c9279669b",
+    "url": "/prac_pokedex/css/app.93793246.css"
+  },
+  {
+    "revision": "8d0613c1e74a97ca1b89",
+    "url": "/prac_pokedex/css/chunk-621c3f2d.07313c52.css"
+  },
+  {
+    "revision": "02c915873ce36e58efe1",
+    "url": "/prac_pokedex/css/chunk-vendors.3cc75806.css"
+  },
+  {
+    "revision": "675fd77a83c167be159c5d98c01bc128",
+    "url": "/prac_pokedex/img/playerf.jpeg"
+  },
+  {
+    "revision": "4e3e984017c148fe6f6e77ddedbd71b9",
+    "url": "/prac_pokedex/img/web.png"
+  },
+  {
+    "revision": "45a2caab553589d7980ce970687d2425",
+    "url": "/prac_pokedex/index.html"
+  },
+  {
+    "revision": "22f2194ef86c9279669b",
+    "url": "/prac_pokedex/js/app.c4a0a6b4.js"
+  },
+  {
+    "revision": "8d0613c1e74a97ca1b89",
+    "url": "/prac_pokedex/js/chunk-621c3f2d.9e06e409.js"
+  },
+  {
+    "revision": "02c915873ce36e58efe1",
+    "url": "/prac_pokedex/js/chunk-vendors.0dc4176e.js"
+  },
+  {
+    "revision": "517f3f8315d46e8efdc89c25ac4fdad0",
+    "url": "/prac_pokedex/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/prac_pokedex/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/prac_pokedex/precache-manifest.a0a520bae258e395bb292bd78dd056c3.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "prac_pokedex"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
